e25615a9443eda0cec45768b4d2f37f495a3e690
aymen3232 <aymen@biborne.com>
2023-04-07 11:57:31 +0200

    Modification du fichier script

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-// Ajouter un message de bienvenue à la console
-console.log('Hello!');
+// Récupération des éléments du formulaire
+const form = document.querySelector('form');
+const nom = document.querySelector('#nom');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
 
-// Demander le nom de l'utilisateur
-var nom = prompt('Quel est ton nom?');
+// Fonction de validation du formulaire
+function validerFormulaire(event) {
+  event.preventDefault(); // Empêche l'envoi du formulaire par défaut
 
-// Afficher un message de salutation personnalisé
-alert('Hello ' + nom + '');
+  // Vérification des champs requis
+  if (nom.value.trim() === '' || email.value.trim() === '' || message.value.trim() === '') {
+    alert('Veuillez remplir tous les champs.');
+    return;
+  }
+
+  // Vérification de l'e-mail
+  const regexEmail = /^\S+@\S+\.\S+$/;
+  if (!regexEmail.test(email.value)) {
+    alert('Veuillez saisir une adresse e-mail valide.');
+    return;
+  }
+
+  // Le formulaire est valide, affichage d'un message de succès
+  alert('Votre message a été envoyé avec succès !');
+  form.reset(); // Réinitialisation du formulaire
+}
+
+// Ajout d'un écouteur d'événement sur la soumission du formulaire
+form.addEventListener('submit', validerFormulaire);
+
