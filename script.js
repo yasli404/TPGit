207af709016968e97fcf41ebfb7272fd066ba881
aymen3232 <aymen@biborne.com>
2023-04-07 11:13:04 +0200

    J'ai ajouter le code dans le fichier script

---
+++ script.js
@@ -0,0 +1,8 @@
+// Ajouter un message de bienvenue à la console
+console.log('Hello!');
+
+// Demander le nom de l'utilisateur
+var nom = prompt('Quel est ton nom?');
+
+// Afficher un message de salutation personnalisé
+alert('Hello ' + nom + '');
